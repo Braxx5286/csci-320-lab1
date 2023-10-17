5d3e4948125f4f21f345abb02f044b52906608a9
Brandon Brockett <bbrockett903@gmail.com>
2023-10-17 20:40:56 +0000

    commit assignment

--- lab1.c
+++ lab1.c
@@ -4,21 +4,30 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lab1.h"
 
 char* readString(char* fileName) {
     char* buffer = NULL;
     size_t len = 0;
-    size_t bytesRead;
+    ssize_t bytesRead;
 
-    // Read a line from the file into the buffer
-    bytesRead = getline(&buffer, &len, fileName);
-    
+    FILE* file = fopen(fileName, "r");
+
+    if (file == NULL) {
+        fprintf(stderr, "An Error While Opening The File Has Occurred.\n");
+        return NULL;
+    }
+
+    bytesRead = getline(&buffer, &len, file);
+
+    fclose(file);
+
     if (bytesRead == -1) {
-        // Error or end of file
         free(buffer);
+        fprintf(stderr, "An Error While Reading The File Has Occurred.\n");
         return NULL;
     } else {
-        // Remove the newline character, if present
         if (bytesRead > 0 && buffer[bytesRead - 1] == '\n') {
             buffer[bytesRead - 1] = '\0';
         }
@@ -28,6 +37,21 @@ char* readString(char* fileName) {
 
 char* mysteryExplode(const char* str) {
     // The length of the string to be returned can be represented by:
-    // (N*(N+1) / 2) + 1
-    
+    // (N*(N+1) / 2) + 1 (given in class)
+    int inputLength = strlen(str); // Integer variable that represents the length of the input string
+    int resultLength = (inputLength * (inputLength + 1) / 2) + 1; // Length of the string to be returned, calculated using equation given in class UwU
+    char* outputStr = (char*)malloc(resultLength); // Allocates memory dynamically for the result string
+
+    outputStr[0] = '\0'; // Initialize the result string as an empty string
+
+    int outputIndex = 0; // Track the current position in the result string
+
+    for (int i = 1; i <= inputLength; i++) { // Loop through the input string
+        for (int j = 0; j < i; j++) { // Copy characters from inputStr to outputStr
+            outputStr[outputIndex++] = str[j];
+        }
+        outputStr[outputIndex] = '\0'; // Update the null-termination of the C string
+    }
+
+    return outputStr;
 }
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
+explode: lab1.o main.o
+	$(CC) -o $@ $?
+
 lab1.o: lab1.c
 	$(CC) -c $?
 
 main.o: main.c
 	$(CC) -c $?
-
-explode: lab1.o main.o
-	$(CC) -o $?
 
 clean:
 		-rm -f *.o
